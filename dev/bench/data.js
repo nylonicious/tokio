41f1f428b1c75a8a7cb41b14521e8efb1c927d08
github-action-benchmark <github@users.noreply.github.com>
2020-11-27 18:38:07 +0000

    add sync_mpsc (cargo) benchmark result for 5e406a7a47699d93fa2a77fb72553600cb7abd0f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1606502249547,
+  "lastUpdate": 1606502286971,
   "repoUrl": "https://github.com/nylonicious/tokio",
   "entries": {
     "rt_multi_threaded": [
@@ -181,6 +181,90 @@ window.BENCHMARK_DATA = {
             "name": "uncontented_unbounded",
             "value": 798864,
             "range": "± 95108",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rajiv.chauhan@gmail.com",
+            "name": "Rajiv Chauhan",
+            "username": "chauhraj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5e406a7a47699d93fa2a77fb72553600cb7abd0f",
+          "message": "macros: fix outdated documentation (#3180)\n\n1. Changed 0.2 to 0.3\r\n2. Changed ‘multi’ to ‘single’ to indicate that the behavior is single threaded",
+          "timestamp": "2020-11-26T19:46:15+01:00",
+          "tree_id": "ac6898684e4b84e4a5d0e781adf42d950bbc9e43",
+          "url": "https://github.com/nylonicious/tokio/commit/5e406a7a47699d93fa2a77fb72553600cb7abd0f"
+        },
+        "date": 1606502286157,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contention_bounded",
+            "value": 7135229,
+            "range": "± 2758347",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_bounded_full",
+            "value": 6807855,
+            "range": "± 2280370",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_unbounded",
+            "value": 6673477,
+            "range": "± 3486523",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_000_medium",
+            "value": 923,
+            "range": "± 211",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_medium",
+            "value": 923,
+            "range": "± 119",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_1_medium",
+            "value": 940,
+            "range": "± 220",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_large",
+            "value": 63424,
+            "range": "± 15085",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_medium",
+            "value": 1084,
+            "range": "± 364",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_bounded",
+            "value": 1164818,
+            "range": "± 220021",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_unbounded",
+            "value": 768327,
+            "range": "± 193530",
             "unit": "ns/iter"
           }
         ]
